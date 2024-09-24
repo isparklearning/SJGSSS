35043d9b3494f58d2c4c8b3ed08e9078806f40da
isparklearning <isparklearning123@gmail.com>
2024-09-24 10:29:44 +0530

    Update 001.py

--- 001.py
+++ 001.py
@@ -29,9 +29,8 @@ else:
 f = ".xlsx"
 e = "Here is the Result of " + c
 if st.button("Submit"):
-    progress_text = "Operation in progress. Please wait."
+    progress_text = "Searching in progress. Please wait."
     my_bar = st.progress(0, text=progress_text)
-
     for percent_complete in range(100):
         time.sleep(0.01)
         my_bar.progress(percent_complete + 1, text=progress_text)
@@ -40,6 +39,7 @@ if st.button("Submit"):
     if d == 1:
         st.warning("Please Select a Valid Class")
     else:
+        st.success('Found!', icon="✅")
         dataframe1 = pd.read_excel(c + f)
         st.write(e)
         st.write(dataframe1)
