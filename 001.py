ee802e415d8465dc27ec0ce10bef9b50819445dc
isparklearning <isparklearning123@gmail.com>
2024-09-25 23:23:36 +0530

    Update 001.py

--- 001.py
+++ 001.py
@@ -52,7 +52,3 @@ if st.button("Submit"):
                         data=template_byte,
                          file_name=c+f,
                          mime='application/octet-stream')
-            st.download_button(label="Click to Download File as PDF",
-                           data=template_byte,
-                           file_name=c+".pdf",
-                           mime='application/octet-stream')
